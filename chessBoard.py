f6b50a89184a8d2be766bf38b658ac762dd5998a
Olamide Ifarajimi <lordelami@gmail.com>
2026-09-16 10:38:47 +0100

    Disabled Logs

--- chessBoard.py
+++ chessBoard.py
@@ -1,7 +1,7 @@
 import copy, sys, logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -  %(levelname)s -  %(message)s')
-# logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 starting_board = {}
 ROWS = "87654321"
